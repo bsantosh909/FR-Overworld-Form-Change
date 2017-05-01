3c9531d9c532c419ab781f9ffedba91b42c3e957
The_Learner <Bsantosh909@gmail.com>
2017-05-01 20:56:16 +0545

    Small text fix

--- src/Therain_trio_script.s
+++ src/Therain_trio_script.s
@@ -23,21 +23,21 @@ release
 end
 
 changed_to_therain:
-msgbox Hoopa_changed_to_therain_string MSG_KEEPOPEN
+msgbox Trio_changed_to_therain_string MSG_KEEPOPEN
 closeonkeypress
 release
 end
 
 changed_to_incarnate:
-msgbox Hoopa_changed_to_incarnate_string MSG_KEEPOPEN
+msgbox Trio_changed_to_incarnate_string MSG_KEEPOPEN
 closeonkeypress
 release
 end
 
-Hoopa_changed_to_therain_string:
+Trio_changed_to_therain_string:
 .byte 0xFD, 0x3, Space, c_, h_, a_, n_, g_, e_, d_, Space, t_, o_, Space, T_, h_, e_, r_, a_, i_, n_, Space, f_, o_, r_, m_, Exclam, Termin
 
-Hoopa_changed_to_incarnate_string:
+Trio_changed_to_incarnate_string:
 .byte 0xFD, 0x3, Space, c_, h_, a_, n_, g_, e_, d_, Space, t_, o_, Space, I_, n_, c_, a_, r_, n_, a_, t_, e_, Space, f_, o_, r_, m_, Exclam, Termin
 
 wont_have_effect_string:
